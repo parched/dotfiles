ea5eb645bc0209d5f4904310352159b16dfb771a
James Duley <james.duley@holmesgroup.com>
2024-10-09 08:37:44 +1300

    Add some windows setup files
    These could be made to be run by chezmoi,
    but I think it's simpler to just manually run them as needed.

---
+++ setup/dev-drive.ps1
@@ -0,0 +1,13 @@
+setx npm_config_cache J:\packages\npm
+
+setx NUGET_PACKAGES J:\packages\nuget
+
+setx VCPKG_DEFAULT_BINARY_CACHE J:\packages\vcpkg
+
+setx PIP_CACHE_DIR J:\packages\pip
+
+setx CARGO_HOME J:\packages\cargo
+
+setx MAVEN_OPTS "-Dmaven.repo.local=J:\packages\maven"
+
+setx GRADLE_USER_HOME "J:\packages\gradle"
