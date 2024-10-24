00a71bc29fa9a0f5c30d59a477a6d37ea720268e
James Duley <jagduley@gmail.com>
2024-10-24 19:57:10 +1300

    Fix missing quotes for dev drive set up

--- setup/dev-drive.ps1
+++ setup/dev-drive.ps1
@@ -1,12 +1,12 @@
-setx npm_config_cache J:\packages\npm
+setx npm_config_cache "J:\packages\npm"
 
-setx NUGET_PACKAGES J:\packages\nuget
+setx NUGET_PACKAGES "J:\packages\nuget"
 
-setx VCPKG_DEFAULT_BINARY_CACHE J:\packages\vcpkg
+setx VCPKG_DEFAULT_BINARY_CACHE "J:\packages\vcpkg"
 
-setx PIP_CACHE_DIR J:\packages\pip
+setx PIP_CACHE_DIR "J:\packages\pip"
 
-setx CARGO_HOME J:\packages\cargo
+setx CARGO_HOME "J:\packages\cargo"
 
 setx MAVEN_OPTS "-Dmaven.repo.local=J:\packages\maven"
 
