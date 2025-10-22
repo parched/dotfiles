161177d32bac5230a16b1e20f36ff405419bb4c2
James Duley <james.duley@holmesgroup.com>
2025-10-22 14:50:48 +1300

    Add some more dev drive cache dirs

--- setup/dev-drive.ps1
+++ setup/dev-drive.ps1
@@ -1,13 +1,14 @@
 setx npm_config_cache "J:\packages\npm"
+setx YARN_CACHE_FOLDER "J:\packages\yarn"
 
 setx NUGET_PACKAGES "J:\packages\nuget"
 
 setx VCPKG_DEFAULT_BINARY_CACHE "J:\packages\vcpkg"
 
 setx PIP_CACHE_DIR "J:\packages\pip"
+setx UV_CACHE_DIR "J:\packages\uv"
 
 setx CARGO_HOME "J:\packages\cargo"
 
 setx MAVEN_OPTS "-Dmaven.repo.local=J:\packages\maven"
-
 setx GRADLE_USER_HOME "J:\packages\gradle"
